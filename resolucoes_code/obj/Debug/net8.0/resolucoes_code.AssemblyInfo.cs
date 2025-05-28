9aecec8d413b868b928bcc31affe639cf0cc6b06
Itzshey <sofiahelene1403@gmail.com>
2025-05-27 21:58:46 -0300

    Resolvendo o problema de operações matemáticas

--- resolucoes_code/obj/Debug/net8.0/resolucoes_code.AssemblyInfo.cs
+++ resolucoes_code/obj/Debug/net8.0/resolucoes_code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("resolucoes_code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea8d133f4f75525e32fc3dcd8668cd08d9862f3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98b55a5cd9c899b2e348406da6638a982cb2c944")]
 [assembly: System.Reflection.AssemblyProductAttribute("resolucoes_code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("resolucoes_code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
